b3b8004dd5886572a155811c232534cc521dd728
shakikk <shakik03@gmail.com>
2023-04-08 01:55:37 +0100

    announcements functioning (needs to be cleaned up)
    announcements now work. Managers can post, everyone can view. Only manager at this moment of time is the testing@testing.com, but this can very easily be changed. The home screen also shows what type of user is currently logged in.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 import SignOutButton from '../Auth/SignOutButton';
+import { auth } from '../Auth/firebase'
 
 function HomePage() {
+    const [ Manager, setManager ] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user && user.email === 'testing@testing.com') {
+                setManager(true);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
     return (
         <div className="container">
-            <h1>Workwise Portal</h1>
+            {Manager ? (
+                <h1>Workwise Portal - Manager</h1>
+            ) : (
+                <h1>Welcome Portal - Employee</h1>
+            )}
             <div>
                 <Link to="/Announcements">
                     <button className="button-style">Announcements</button>
